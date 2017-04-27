22470dcf35d59608f23fdb6e19024e91fc3c62c1
Bruce Khin <brucekhin_home@Bruces-iMac.local>
2017-04-26 17:18:14 -0700

    Updated Review Graph Fragment
    Updated Review Graph Fragment

--- app/src/main/java/com/theearthworks/asthmatrack_st/SplashActivity.java
+++ app/src/main/java/com/theearthworks/asthmatrack_st/SplashActivity.java
@@ -1,14 +1,9 @@
 package com.theearthworks.asthmatrack_st;
 
 import android.app.Activity;
-import android.content.res.Configuration;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class SplashActivity extends Activity {
 
@@ -18,7 +13,7 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_review_attacks);
+        setContentView(R.layout.review_frag);
 
     }
 
