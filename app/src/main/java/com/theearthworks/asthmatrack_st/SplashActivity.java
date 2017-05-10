5d6176da71af672f48c0bcb997995c4b2130ed22
Bruce Khin <brucekhin_home@Bruces-iMac.local>
2017-05-09 21:56:43 -0700

    Add Sleep
    Add Sleep

--- app/src/main/java/com/theearthworks/asthmatrack_st/SplashActivity.java
+++ app/src/main/java/com/theearthworks/asthmatrack_st/SplashActivity.java
@@ -13,7 +13,7 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_mood2);
+        setContentView(R.layout.activity_add_sleep3);
 
     }
 }
